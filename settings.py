e19787bf22b9edac5feb907ef59c65da0081c26f
HeIpCenter <153394881+HeIpCenter@users.noreply.github.com>
2024-12-26 14:50:08 +0800

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,10 @@
+api_id = "20817924"
+api_hash = "e198956b15c11914ea644d0943d76ac8"
+
+admin = [6987171667]
+bot_token = "7608251696:AAFLmoJxd_zL00LDz-aUpb0BL6NUhnSf7lw"
+bot_username = "qpqpqpqbbot"
+showperpage = 50
+
+path = "/home/indones9/public_html/bantuan/xcoba/"
+url = "http://localhost:8012/phis_auto/"
